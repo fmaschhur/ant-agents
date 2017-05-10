bf2e47b95b584134bfffdf69cf4d82d5172f7cbb
Florian <f.steffens@campus.tu-berlin.de>
2017-05-10 12:38:36 +0200

    lel fuk u mux

---
+++ ants.py
@@ -0,0 +1,18 @@
+class Ants(object):
+
+    def init(self, currpos, lastpos, carrfood, nestdist):
+        self.Currpos = currpos
+        self.Lastpos = lastpos
+        self.carrfood = carrfood
+        self.Nestdist = nestdist
+
+    def move():
+        pass
+    def setpherom(edge):
+        pass
+    def getFood():
+        pass
+
+
+
+
