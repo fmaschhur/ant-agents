65854cb4fb1d20c7700d5bec784dc86d2eead96f
f_maschhur@gitlab.tubit.tu-berlin.de <faraz@hrlab.de>
2017-05-15 14:22:37 +0200

    graph.py sorted, still no function

--- graph.py
+++ graph.py
@@ -5,8 +5,13 @@ class Graph(object):
 
     def init(self, x, y, nest):
         self.nodes = self.create_nodes(x, y)
-        self.edges =
+        self.edges = self.create_edges()
         self.nest = nest
         self.antcount = 0
+        return self
 
     def create_nodes(self, x, y):
+        return
+
+    def create_edges(self):
+        return
