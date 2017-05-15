e49310db519c0013a5458f963ec34352300e91af
f_maschhur@gitlab.tubit.tu-berlin.de <faraz@hrlab.de>
2017-05-15 14:59:48 +0200

    graph.py sorted and extended, still no function

--- graph.py
+++ graph.py
@@ -1,17 +1,33 @@
 import node
 import edge
+import random
 
 class Graph(object):
 
-    def init(self, x, y, nest):
+    def init(self, x, y):
         self.nodes = self.create_nodes(x, y)
         self.edges = self.create_edges()
-        self.nest = nest
+        self.nest = self.choose_nest(x, y, self.nodes)
         self.antcount = 0
         return self
 
     def create_nodes(self, x, y):
-        return
+        nodes = {}
+        for x in range(0, x+1):
+            for y in range(0, y+1):
+                add_me = node.init(0, None)
+                nodes[(x, y)] = add_me
+        return nodes
 
     def create_edges(self):
-        return
+        edges = []
+        for (x, y) in self.nodes:
+            if x == 1:          # not left
+                if y == 1:      # not up
+                    edges.append(edge.init(self.nodes(x, y), self.nodes(x, (y + 1))))
+                    edges.append(edge.init(self.nodes(x, y), self.nodes((x + 1), y)))
+
+    def choose_nest(self, x, y, nodes):
+        a = random.randint(0, x)
+        b = random.randint(0, y)
+        return nodes[(a, b)]
