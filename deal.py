5cffdff00e77db4b0426196ddd4154b082bff563
karl <clemens.l.kuske@win.tu-berlin.de>
2017-05-31 20:24:13 +0200

    Grundgerüst steht

--- deal.py
+++ deal.py
@@ -1,16 +1,18 @@
 from job import Job
 from math import inf
 
-
+# Speichert die Deals der einzelnen Agenten für die Initiatoren ab
 class Deal(object):
     def __init__(self, agent, job):
-        self.agent = agent
-        self.job = job
-        self.bid = inf
-        self.status_initiator = 0
-        self.pre_bid = 1
-        job.initiator.deals.append(self)
+        self.agent = agent # zugehöriger Agent
+        self.job = job # Job für den der Deal ist
+        self.bid = inf # Eingegebenes Angebot
+        self.status_initiator = 0 # Status aus sich des Initiators
+        self.pre_bid = 1 # Status aus sich des Agenten als Boolean
+        job.initiator.deals.append(self)  # Der liste des Initiators hinzufügen
 
+
+    # Ausgabe und eingabe für den Status aus sicht des Initiators
     def is_definitive_rejected(self):
         return 1 if self.status_initiator == 0 else 0
 
