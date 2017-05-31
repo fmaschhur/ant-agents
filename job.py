733880a8e7933c8adbb7f90e37302d8a18489256
karl <clemens.l.kuske@win.tu-berlin.de>
2017-05-31 20:25:46 +0200

    Grundgerüst steht

--- job.py
+++ job.py
@@ -8,7 +8,7 @@ class Job(object):
         self.id = id
         self.time = time
         self.destination = destination
-        self.status = 0
-        self.initiator = None
+        self.status = 0 # Status: 0: Nicht begonne, 1: Steht bereit für Deals, 2: Wird angefahren 3: Abgeschlossen
+        self.initiator = None # Initiator der für job zuständig ist
 
 
